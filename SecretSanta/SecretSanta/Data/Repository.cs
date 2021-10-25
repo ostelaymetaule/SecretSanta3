1378e1c60a8b728e0f70fc2fa07a14673bac785b
Alexander Elgin <alexander.elgin@hotmail.com>
2021-10-25 21:56:39 +0200

    Saving to repository

--- SecretSanta/SecretSanta/Data/Repository.cs
+++ SecretSanta/SecretSanta/Data/Repository.cs
@@ -11,21 +11,51 @@ namespace SecretSanta.Data
     internal class Repository
     {
         private Microsoft.Extensions.Caching.Memory.IMemoryCache _memoryCache;
+        private LiteDB.ILiteDatabase _db;
+        //todo: add lookup for cached data first?
 
         public Repository()
         {
             _memoryCache = new MemoryCache(new MemoryCacheOptions()
             {
-                
+
             });
+            var dbPath = Path.Combine(Environment.CurrentDirectory, "rep", "myDb.db");
+            _db = new LiteDB.LiteDatabase(dbPath);
         }
+        /// <summary>
+        /// Save that chat group to db 
+        /// </summary>
+        /// <param name="chatgroup"></param>
         public void Save(ChatGroup chatgroup)
         {
-
+            var chatGroupCollection = _db.GetCollection<ChatGroup>(nameof(chatgroup));
+            var exists = chatGroupCollection.Exists(x => x.ChatId == chatgroup.ChatId);
+            if (!exists)
+            {
+                chatgroup.Status = Status.init; //todo: move to constructor?
+            } 
+            chatGroupCollection.Upsert(chatgroup);
+            chatGroupCollection.EnsureIndex(x => x.ChatId);
         }
+        /// <summary>
+        /// Gets all saved chats
+        /// </summary>
+        /// <returns></returns>
         public List<ChatGroup> ReadAll()
         {
-            return new List<ChatGroup>();
+            var chatGroupCollection = _db.GetCollection<ChatGroup>("chatgroup");
+            return chatGroupCollection.Query().Select(x => x).ToList();
+        }
+        /// <summary>
+        /// Returns a single chatGroup by Chatid
+        /// </summary>
+        /// <param name="chatId">telegram intern chatId</param>
+        /// <returns>First appearence of the chatGroup object with a given chatId</returns>
+        public ChatGroup GetById(long chatId)
+        {
+            var chatGroupCollection = _db.GetCollection<ChatGroup>("chatgroup");
+            return chatGroupCollection.Query().Where(x => x.ChatId == chatId).Select(x => x).FirstOrDefault();
         }
 
 
